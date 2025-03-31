8e3623cb94b5cd0f6d5400c903996615219e30a6
cubewebs <juanjoseaos@hotmail.com>
2025-03-31 12:05:29 +0100

    Show custom components

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import { AccentButton } from "./components/atoms/accent-btn"
+import { BorderedButton } from "./components/atoms/bordered-btn"
+import { ToggleSwitch } from "./components/atoms/toggle-switch"
 
 export const TodoApp = () => {
+    const [checked, setChecked] = useState(false)
+    
   return (
     <>
       <h1>Todo App</h1>
+      <div>
+        <div>
+            <AccentButton type={'button'} children={'Add'} />
+        </div>
+        <div>
+            <BorderedButton type={'button'} children={'Add'} />
+        </div>
+        <div>
+            <ToggleSwitch checked={checked} onChange={setChecked} />
+        </div>
+      </div>
     </>
   )
 }
